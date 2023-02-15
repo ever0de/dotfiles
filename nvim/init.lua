8a0eae062e3f0f8ff638bc5757f8aee9f5c990b3
Jiseok CHOI <jiseok.dev@gmail.com>
2023-02-15 16:32:30 +0900

    `[nvim]`: Add `impatient` plugin

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,3 +3,8 @@ require("plugins.setup")
 
 -- Settings
 require("settings")
+
+-- Plugins
+
+-- <https://github.com/lewis6991/impatient.nvim>
+require("impatient")
--- nvim/lua/plugins/setup.lua
+++ nvim/lua/plugins/setup.lua
@@ -27,6 +27,8 @@ end
 return require("packer").startup(function(use)
 	use "wbthomason/packer.nvim"
 
+	use "lewis6991/impatient.nvim"
+
 	-- Automatically set up your configuration after cloning packer.nvim
 	-- Put this at the end after all plugins
 	if packer_bootstrap then
